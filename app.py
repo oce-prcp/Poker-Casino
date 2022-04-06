b8c62175118bec8d88366c33f2283d54984ab79d
BlueDragon1310 <94532496+BlueDragon1310@users.noreply.github.com>
2022-04-06 14:34:54 +0200

    avancement

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def welcome():
-    return "Hello world."
+    return render_template('index.html')
 
 
 @app.route('/hello/<name>', methods=['GET'])
