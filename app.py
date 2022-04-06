621283b6e99376123d935aeea9dcc3d95434bd90
BlueDragon1310 <94532496+BlueDragon1310@users.noreply.github.com>
2022-04-06 15:05:33 +0200

    ..

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 from flask import Flask, url_for, render_template, request
-=======
-from flask import Flask, render_template
->>>>>>> 0313cbfb285287374efb01cc19d37e82a361a767
 
 app = Flask(__name__)
 
 @app.route('/')
-<<<<<<< HEAD
-def welcome():
-=======
+
 def index() :
->>>>>>> 0313cbfb285287374efb01cc19d37e82a361a767
     return render_template('index.html')
 
 if __name__ == '__main__':
