ca5d74d4d2470d49819500dfb53c646dacedf652
Mathieu Medinas <mathieu.medinas@gmail.com>
2022-04-06 20:17:38 +0200

    jsp

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from crypt import methods
-from flask import Flask, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session
 from main import *
+import random
 app = Flask(__name__)
 app.secret_key="secret_key_sexe"
 
@@ -13,17 +13,38 @@ def accueil():
 @app.route('/', methods=['POST'])
 def index() :
     sexe = request.form['sexe']
+    age = int(request.form['age'])
 
-    if sexe.lower() == "homme":
-        session["message"] = "Vous êtes un homme"
-        return render_template('index.html')    
+    sexes = ["femme", "homme"]
+    if sexe.lower() in sexes:
+        if age >= 18:
+            random_id = random.randint(10000, 20000)
+            session['id'] = random_id
+            return redirect('/play', 302)
+        else:
+             return render_template('index.html', message="Vous n'êtes pas majeur")
+    else:
+        return render_template('index.html', message="Les autres genre ne sont pas accepté")
 
-    elif sexe.lower() == "femme":
-        session["message"] = "Vous êtes une femme"
-        return render_template('index.html')
-    
+@app.route('/play', methods=['GET'])
+def play():
+    if session['id'] == None:
+       return redirect('/', 302)
+
+    session['lol'] = lol
+
+    return render_template('play.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    if session['id'] != None:
+        session['id'] = None
+        return redirect('/', 302)
+    else:
+        return redirect('/', 302)
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
