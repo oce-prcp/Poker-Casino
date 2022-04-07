2ab3bb38ce018f7963c49069956211e0f415170c
rayhad <hadidrayann93@gmail.com>
2022-04-07 14:11:43 +0200

    rat_commit1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-from main import *
+from main import premier_tirage
 import random
 app = Flask(__name__)
 app.secret_key="secret_key_sexe"
@@ -20,23 +20,34 @@ def index() :
         if age >= 18:
             random_id = random.randint(10000, 20000)
             session['id'] = random_id
-            return redirect('/play', 302)
+            return redirect('/bankroll', 302)
         else:
              return render_template('index.html', message="Vous n'êtes pas majeur")
     else:
         return render_template('index.html', message="Les autres genre ne sont pas acceptés")
 
 
-@app.route('/play', methods=['GET'])
-def play():
-
-    bankroll = request.form.get['bankroll']
-    mise = request.form.get['mise']
-
-
-    if session['id'] == None:
-       return redirect('/', 302)
+@app.route('/bankroll',methods=['GET', 'POST'])
+def bankroll():
+    confirmation = 0
+    if request.method == "POST":
+        bankroll = int(request.form.get('bankroll'))
+        mise = int(request.form.get('mise'))
+        while confirmation != 1:
+            if bankroll <= 0 or mise <= 0:
+                 return render_template('bankroll.html', message="Veuillez inserer une valeur superieur a 0")
+            elif bankroll >= 1 and mise >= 1:
+                confirmation += 1
+                return redirect('/play', 302)
+        
+        if session['id'] == None:
+            return redirect('/', 302)
     
+    return render_template('bankroll.html')
+
+@app.route('/play',methods=['GET','POST'])
+
+def play():
     return render_template('play.html')
 
 
